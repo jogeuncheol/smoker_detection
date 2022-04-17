ba49b35d0fa7c422c11b7831bb41e9c404893a3a
jogeuncheol <84510674+jogeuncheol@users.noreply.github.com>
2022-04-17 14:21:12 +0900

    [fix] Tracker.set_data :: args -> list
    Tracker 클래스의 set_data 메소드의 args -> list

--- jgc/track_class.py
+++ jgc/track_class.py
@@ -33,25 +33,30 @@ class Tracker:
     def __str__(self):
         return str(self.bbox) + ", " + str(self.outer_roi_flag) + ", " + str(self.ROI_R) + ", " + str(self.outer_roi) + ", " + str(self.inner_roi)
 
-    def set_data(self, box, f, r, outer_roi, inner_roi):
-        self.bbox = box
-        self.outer_roi_flag = f
-        self.ROI_R = r
-        self.outer_roi = outer_roi
-        self.inner_roi = inner_roi
+    def set_data(self, t_list):
+        self.bbox = t_list[0]
+        self.outer_roi_flag = t_list[1]
+        self.ROI_R = t_list[2]
+        self.outer_roi = t_list[3]
+        self.inner_roi = t_list[4]
 
 
 if __name__ == "__main__":
     t_dict = TDict()
+    dic = t_dict.t_dict
     for t_id in range(1, 10):
         t_value = Tracker()
-        _box = [100, 100, 20, 20]
-        flag = 0
-        R = 30
-        or_box = [50, 50, 100, 100]
-        ir_box = [80, 80, 40, 40]
-        t_value.set_data(_box, flag, R, or_box, ir_box)
+        t_list = []
+        t_list.append([t_id, 100, 20, 20])
+        t_list.append(0)
+        t_list.append(30)
+        t_list.append([50, 50, 100, 100])
+        t_list.append([80, 80, 40, 40])
+        t_value.set_data(t_list)
         t_dict.add_dict(t_id, t_value)
     t_dict.items()
     t_dict.del_dict(4)
     t_dict.items()
+    print("key is 1, value is {}".format(t_dict.t_dict[1]))
+    print("key is 1, value.bbox is {}".format(t_dict.t_dict[1].bbox))
+    print("key is 1, value.bbox[0] is {}".format(t_dict.t_dict[1].bbox[0]))
